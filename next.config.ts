c3e4f4bbd57497f4ddcc44e4e90b7db2c584f1aa
leandrofrancov48-alt <leandrofrancov48@gmail.com>
2026-06-02 17:01:46 -0300

    kj

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async redirects() {
-    return [
-      {
-        source: "/",
-        destination: "/ingresos",
-        permanent: false, // 302 temporal
-      },
-    ];
-  },
+  /* config options here */
 };
 
 export default nextConfig;
